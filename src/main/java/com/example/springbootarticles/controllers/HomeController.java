81cb321e6d8692f8082113d49248afb4706e67d8
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-10-08 14:45:15 +0300

    Changes in start redirect to swagger with context path for Tomcat

--- src/main/java/com/example/springbootarticles/controllers/HomeController.java
+++ src/main/java/com/example/springbootarticles/controllers/HomeController.java
@@ -3,12 +3,16 @@ package com.example.springbootarticles.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class HomeController {
     @GetMapping("/")
-    public RedirectView welcomePage() {
+    public RedirectView welcomePage(UriComponentsBuilder uriComponentsBuilder) {
         // Redirect to the swagger ui
-        return new RedirectView("/swagger-ui.html");
+        // As application deployed on Tomcat, the context path is added to the url
+        // So, we need to get the context path and append it to the url
+        String contextPath = uriComponentsBuilder.build().getPath();
+        return new RedirectView(contextPath + "/swagger-ui.html");
     }
 }
