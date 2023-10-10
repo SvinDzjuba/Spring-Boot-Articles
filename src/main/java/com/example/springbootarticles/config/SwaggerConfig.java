ecbf01b946edcefec73a271c8bdbeb13686d9059
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-10-10 14:22:09 +0300

    Fixed swagger server path for https

--- src/main/java/com/example/springbootarticles/config/SwaggerConfig.java
+++ src/main/java/com/example/springbootarticles/config/SwaggerConfig.java
@@ -6,10 +6,13 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
@@ -22,6 +25,7 @@ public class SwaggerConfig {
                 .info(apiInfo())
                 .components(new Components()
                         .addSecuritySchemes(SCHEME_NAME, createSecurityScheme()))
+                .servers(Collections.singletonList(new Server().url("https://dzalex.sytes.net/spring-boot-articles")))
                 .addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME));
     }
 
